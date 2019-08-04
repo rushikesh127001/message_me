3646e2074480f8802eaf5ca87bf280f1cc12cf75
Rushikesh127001 <chasrushikesh@gmail.com>
2019-08-04 19:30:36 +0530

    added chatroom channel ,broadcasting messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,8 @@ class MessagesController < ApplicationController
         message= current_user.messages.build(message_params)
 
         if message.save
-            redirect_to root_path
+            ActionCable.server.broadcast "chatroom_channel",
+                                         mod_message: message_render(message) 
 
         end    
 
@@ -16,5 +17,9 @@ class MessagesController < ApplicationController
 
     def message_params
         params.require(:message).permit(:body)
+    end    
+
+    def message_render(message)
+        render(partial: 'message',locals:{message: message} )
     end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   post 'login', to: 'session#create'
   delete 'logout', to: 'session#destroy'
   post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
